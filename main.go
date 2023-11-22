b139e2883707834c8d9688f6b4c7a3846455ab9d
rakhil <rakhil@cisco.com>
2023-11-22 20:44:11 +0530

    Added JSON functionality and modified some files

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 
-	"github.com/BurntSushi/toml"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
 )
@@ -20,25 +21,39 @@ func main() {
 
 	flag.Parse()
 
-	//-
+	// -
 
 	bundle := i18n.NewBundle(language.English)
-	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
-	bundle.LoadMessageFile("active.en.toml")
-	bundle.LoadMessageFile("active.es.toml")
+	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+
+	// Load JSON message files directly
+	if _, err := bundle.LoadMessageFile("translations/en_US.json"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading en bundle: %v\n", err)
+		os.Exit(1)
+	}
+	if _, err := bundle.LoadMessageFile("translations/es_SP.json"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading es bundle: %v\n", err)
+		os.Exit(1)
+	}
 
 	localizer := i18n.NewLocalizer(bundle, lang)
 
-	buying := localizer.MustLocalize(&i18n.LocalizeConfig{
+	// buying := localizer.MustLocalize(&i18n.LocalizeConfig{
+	// 	DefaultMessage: &i18n.Message{
+	// 		ID:    "DefaultMessage",
+	// 		One:   "You're buying 1 cookie.",
+	// 		Other: "You're buying {{.PluralCount}} cookies.",
+	// 	},
+	// 	PluralCount: count,
+	// })
+
+	greeting := localizer.MustLocalize(&i18n.LocalizeConfig{
 		DefaultMessage: &i18n.Message{
-			ID:    "BuyingCookies",
-			One:   "You're buying 1 cookie.",
-			Other: "You're buying {{.PluralCount}} cookies.",
+			ID:    "Greeting",
 		},
 		PluralCount: count,
 	})
 
-	// buying := "" // "You're buying %d cookies\n", count
-
-	fmt.Printf("%s\n", buying)
+	// fmt.Printf("%s\n", buying)
+	fmt.Printf("%s\n", greeting)
 }
